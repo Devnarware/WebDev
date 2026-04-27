73915dc6060b93c6a1956c7651feaa025e1e2228
Devendra Kumar Narware <devn2405@gmail.com>
2026-04-27 08:47:20 +0530

    Added react folder

---
+++ react/website/my-react-app/src/Card.jsx
@@ -0,0 +1,19 @@
+
+
+function Card(){
+
+    return(
+        <div className = "card">
+
+            <img src="" alt="Profile picture" />
+            <h2>Dev Narware</h2>
+            <p>I am a 2nd year CSE students</p>
+
+        </div>
+    );
+
+
+}
+
+
+export default Card ;
