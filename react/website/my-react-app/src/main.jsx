78670109fb1d8be40533b4d098f726f556677955
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-01 20:00:00 +0530

    Work done on May 1

--- react/website/my-react-app/src/main.jsx
+++ react/website/my-react-app/src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+const myele = (
+
+  <>
+    <p>hello</p>
+    <a href="https://google.com"></a>
+  </>
+
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
