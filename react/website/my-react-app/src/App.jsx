f932015a479869bec87fb4d181e84c10b5b1dcde
Devendra Kumar Narware <devn2405@gmail.com>
2026-05-04 08:02:34 +0530

    refactor: update App and Button components with styling comments

--- react/website/my-react-app/src/App.jsx
+++ react/website/my-react-app/src/App.jsx
@@ -20,3 +20,12 @@ function App(){
 }
 
 export default App
+
+
+// WAYS TO STYLE A COMPONENT IN REACT
+
+// EXTERNAL CSS :- it is for global value and it is used for styling multiple components(good for small projects) eg:- index.css
+
+// MODULE CSS :- it is for local value and it is used for styling single component(good for large projects) eg:- Button.module.css
+
+// INLINE CSS :- it is for local value and it is used for styling single component(good for large projects) eg:- style={{color:"red"}} in Button.jsx
--- react/website/my-react-app/src/Button.jsx
+++ react/website/my-react-app/src/Button.jsx
@@ -1,8 +1,18 @@
 
-function Button(){
+function Button() {
 
-    return(
-        <button className="button">Click Me</button>
+    // intline css
+    // const styles = {
+    //     border: "none",
+    //     borderRadius: "5px",
+    //     padding: " 7px 15px",
+    //     backgroundColor: "rgba(8, 182, 182, 0.843)",
+    //     cursor: "pointer"
+
+    // }
+
+    return (
+        <button>Click Me</button>
     );
 }
 
